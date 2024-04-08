003583aee221ed09b192841dfb5840b8ed6ad9e9
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-04-08 18:59:03 +0800

    init

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div>hello</div>
+  <n-button type="primary" size="medium">Why it's green</n-button>
 </template>
+
+<script setup>
+import { NButton } from "naive-ui";
+throw new Error("err");
+</script>
+
+<style lang="scss" scoped></style>
---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 这个页面会在应用中未捕获的错误发生时显示 -->
+  <div class="error-page">
+    <el-result
+      :status="404"
+      title="页面出错了"
+      sub-title="当前访问的页面不存在"
+    >
+      <template #extra>
+        <el-button type="primary" @click="backToHome"></el-button> </template
+    ></el-result>
+  </div>
+</template>
+
+<script setup>
+const backToHome = ()=>{
+    
+}
+</script>
+
+<style lang="scss" scoped></style>
